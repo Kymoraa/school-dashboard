7fe80d0f70a1a0154e3f1f4c3921cdf0f1c7e4ee
Kymoraa <jmoraa55@gmail.com>
2023-05-09 17:19:04 +0300

    SD-001: Update the student profile

--- lib/screens/students.dart
+++ lib/screens/students.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:school_dashboard/screens/student_profile.dart';
 import 'package:school_dashboard/themes/text_theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:developer' as console;
 
 class StudentsScreen extends StatefulWidget {
@@ -13,22 +12,10 @@ class StudentsScreen extends StatefulWidget {
 }
 
 class _StudentsScreenState extends State<StudentsScreen> {
-  late DatabaseReference databaseReference;
-  var generatedColor = Random().nextInt(Colors.primaries.length);
-  List<dynamic> allStudents = [];
-
-  Future<List> getAllStudents() async {
-    final databaseInstance = FirebaseDatabase.instance;
-    final fruitDetails = await databaseInstance.ref("all-students").once();
-    setState(() {
-      allStudents = fruitDetails.snapshot.value as List;
-    });
-    return allStudents;
-  }
+  final fireStore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
-    getAllStudents();
     return Scaffold(
       appBar: AppBar(
         title: const Text('All Students'),
@@ -45,46 +32,61 @@ class _StudentsScreenState extends State<StudentsScreen> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: allStudents.length,
-        itemBuilder: (BuildContext context, int index) {
-          var studentFullName = allStudents[index]['name'];
-          List studentSplitName = studentFullName.split(' ');
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: (){ //console.log(index.toString());
-                Navigator.of(context, rootNavigator: true).push(
-                  MaterialPageRoute(
-                    builder: (_) => StudentProfile(
-                      studentID: allStudents[index]['id'],
-                      studentName: allStudents[index]['name'],
-                      studentBalance: allStudents[index]['balance'],
-                      studentGrade: allStudents[index]['grade'],
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: fireStore.collection('students').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: Text('No students to display'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final student = snapshot.data!.docs[index].data();
+              var studentFullName = student['name'];
+              List studentSplitName = studentFullName.split(' ');
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                      MaterialPageRoute(
+                        builder: (_) => StudentProfile(
+                          studentID: student['id'],
+                          studentName: student['name'],
+                          studentGrade: student['grade'],
+                          studentReg: student['regNumber'],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blue[200],
+                        child: Text(
+                          '${studentSplitName[0][0]}${studentSplitName[1][0]}',
+                          style: context.bodyText2.copyWith(color: Colors.white),
+                        ),
+                      ),
+                      title: Text(
+                        student['name'],
+                        style: context.bodyText1.copyWith(color: Colors.black),
+                      ),
+                      subtitle: Text(
+                        'Registration Number: ${student['regNumber']}',
+                        style: context.bodyText2.copyWith(color: Colors.grey),
+                      ),
                     ),
-                  ),
-                );
-              },
-              child: Card(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.blue[200],
-                    child: Text(
-                      '${studentSplitName[0][0]}${studentSplitName[1][0]}',
-                      style: context.bodyText2.copyWith(color: Colors.white),
-                    ),
-                  ),
-                  title: Text(
-                    allStudents[index]['name'],
-                    style: context.bodyText1.copyWith(color: Colors.black),
-                  ),
-                  subtitle: Text(
-                    'Student ID: ${allStudents[index]['id']}',
-                    style: context.bodyText2.copyWith(color: Colors.grey),
                   ),
                 ),
-              ),
-            ),
+              );
+            },
           );
         },
       ),
